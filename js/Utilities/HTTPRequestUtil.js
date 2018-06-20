4476844428692103e1c3b5d3075fd9b501fdca52
Jesper K <jesp6763@edu.campusvejle.dk>
2018-06-20 10:43:15 +0200

    Made some simple HTTPRequest utilities

---
+++ js/Utilities/HTTPRequestUtil.js
@@ -0,0 +1,40 @@
+class HTTPRequestUtil
+{
+    static _Request(methodType, url, async = true, requestDoneCallback = null, mimeType = "application/json")
+    {
+        let xhttpRequest = new XMLHttpRequest();
+        xhttpRequest.overrideMimeType(mimeType);
+        xhttpRequest.open(methodType, url, async)
+
+        xhttpRequest.onreadystatechange = function(){
+            if(xhttpRequest.readyState == 4){
+                if(xhttpRequest.status == "200"){
+                    requestDonecallback(xhttpRequest.responseText);
+                }
+            }
+        }
+
+        xhttpRequest.send(null);
+    }
+
+    /**
+     * Creates a new asynchronous XMLHttpRequest.
+     * Returns the response text.
+     * @param {string} methodType The method type of the request. GET or POST.
+     * @param {string} mimeType The mime type. 
+     */
+    static RequestAsync(methodType, url, requestDoneCallback, mimeType = "application/json")
+    {
+        this._Request(methodType, url, true, requestDoneCallback, mimeType);
+    }
+
+    /**
+     * Creates a new synchronous XMLHttpRequest.
+     * Returns the response text.
+     * @param {string} methodType The method type of the request. GET or POST.
+     * @param {string} mimeType The mime type. 
+     */
+    static RequestSync(methodType, url, mimeType = "application/json"){
+        this._Request(methodType, url, false, null, mimeType);
+    }
+}
